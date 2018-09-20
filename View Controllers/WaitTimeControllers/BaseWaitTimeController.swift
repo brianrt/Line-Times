0439617bb6a13deefd73fb7783ed3c9647ebdfaa
Brian Thompson <brthomp2@illinois.edu>
2018-09-19 21:32:33 -0700

    Completed task of pushing entry submission logic to backend

--- View Controllers/WaitTimeControllers/BaseWaitTimeController.swift
+++ View Controllers/WaitTimeControllers/BaseWaitTimeController.swift
@@ -20,12 +20,9 @@ class BaseWaitTimeController: UIViewController, UITextFieldDelegate, UITextViewD
     var sv: UIView!
     var ref = Database.database().reference()
     var defaults: UserDefaults!
-    var radius: CLLocationDistance = 50.0 //meters
-    var timeInterval = 1800.0 //seconds
     let locationManager = CLLocationManager()
     var venueLocation: CLLocation!
     var currentLocation: CLLocationCoordinate2D!
-    var timeStampUrl = URL(string: "https://us-central1-time-crunch-e109e.cloudfunctions.net/getTimeStamp")
     var userSubmitEntryUrl = URL(string: "https://us-central1-time-crunch-e109e.cloudfunctions.net/app/userSubmitEntry")
 
     
@@ -75,85 +72,65 @@ class BaseWaitTimeController: UIViewController, UITextFieldDelegate, UITextViewD
         return true
     }
     
-    //Convert address to coordinate
-    func getCoordinate( addressString : String, completionHandler: @escaping(CLLocationCoordinate2D, NSError?) -> Void ) {
-        let geocoder = CLGeocoder()
-        geocoder.geocodeAddressString(addressString) { (placemarks, error) in
-            if error == nil {
-                if let placemark = placemarks?[0] {
-                    let location = placemark.location!
-                    
-                    completionHandler(location.coordinate, nil)
-                    return
-                }
-            }
-            completionHandler(kCLLocationCoordinate2DInvalid, error as NSError?)
-        }
-    }
-    
     @IBAction func submitPressed(_ sender: Any) {
         if(locationIsEnabled){
-            while(currentLocation == nil){
-                print(currentLocation)
-            }
-            print("got location!")
-            print(currentLocation)
-            let currentUser = Auth.auth().currentUser
-            currentUser?.getIDTokenForcingRefresh(true, completion: { (tokenID, error) in
-                if let error = error {
-                    self.displayAlert(message: error.localizedDescription)
-                    return
-                }
-                // Send token to backend via HTTPS
-                var request = URLRequest(url: self.userSubmitEntryUrl!)
-                request.allHTTPHeaderFields = ["Authorization" : "Bearer " + tokenID!]
-                request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-                request.addValue("application/json", forHTTPHeaderField: "Accept")
-                request.httpMethod = "POST"
-                
-                // Build up parameters
-                let parameters = self.buildParameters()
-                let jsonData = try? JSONSerialization.data(withJSONObject: parameters)
-                request.httpBody = jsonData
-                
-                //Send the request
-                let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
-                    do {
-                        let json = try JSONSerialization.jsonObject(with: data!) as? NSDictionary
-                        let error = (json!.value(forKey: "error") as! Bool) //If there was an error or not
-                        if (error) {
-                            let title = (json!.value(forKey: "title") as! String) //message response title
-                            let message = (json!.value(forKey: "message") as! String) //message response message
-                            self.displayAlert(title: title, message: message)
-                        } else {
-                            let entryCount = (json!.value(forKey: "entryCount") as! Int)
-                            self.displayAlert(title: "Success", message: "Entry count: \(entryCount)")
-                        }
-                    } catch {
-                        print("Error deserializing JSON: \(error)")
-                    }
-                }
-                
-                task.resume()
-                
-            })
+            sv = UIViewController.displaySpinner(onView: self.view) //Start spinner
+            let start = CFAbsoluteTimeGetCurrent()
+            var elapsed = CFAbsoluteTimeGetCurrent() - start
             
-//            sv = UIViewController.displaySpinner(onView: self.view)
-//            self.ref.child("Categories").child(categoryType).child(name).child("Address").observeSingleEvent(of: .value) { (snapshot) in
-//                //Get the address and convert to coordinate
-//                let address = snapshot.value as! String
-//                self.getCoordinate(addressString: address, completionHandler: { (coordinate, error) in
-//                    if error == nil {
-//                        //Verify user is within range of restaurant
-//                        print(coordinate)
-//                        self.venueLocation = CLLocation(latitude: coordinate.latitude, longitude: coordinate.longitude)
-//                        self.locationManager.requestLocation()
-//                    } else {
-//                        self.displayAlert(message: "This location has an issue right now.")
-//                        UIViewController.removeSpinner(spinner: self.sv)
-//                    }
-//                })
-//            }
+            //Wait at most 10 seconds for location, otherwise time out
+            while(currentLocation == nil && elapsed < 5.0) {
+                elapsed = CFAbsoluteTimeGetCurrent() - start
+                print(elapsed)
+            }
+            if(elapsed >= 5.0){
+                displayAlert(message: "Timed out finding your location, please try again later.")
+            } else {
+                print("got location!")
+                print(currentLocation)
+                let currentUser = Auth.auth().currentUser
+                currentUser?.getIDTokenForcingRefresh(true, completion: { (tokenID, error) in
+                    if let error = error {
+                        self.displayAlert(message: error.localizedDescription)
+                        return
+                    }
+                    // Send token to backend via HTTPS
+                    var request = URLRequest(url: self.userSubmitEntryUrl!)
+                    request.allHTTPHeaderFields = ["Authorization" : "Bearer " + tokenID!]
+                    request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+                    request.addValue("application/json", forHTTPHeaderField: "Accept")
+                    request.httpMethod = "POST"
+                 
+                    // Build up parameters
+                    let parameters = self.buildParameters()
+                    let jsonData = try? JSONSerialization.data(withJSONObject: parameters)
+                    request.httpBody = jsonData
+                 
+                    //Send the request
+                    let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+                        do {
+                            let json = try JSONSerialization.jsonObject(with: data!) as? NSDictionary
+                            let error = (json!.value(forKey: "error") as! Bool) //If there was an error or not
+                            if (error) {
+                                let title = (json!.value(forKey: "title") as! String) //message response title
+                                let message = (json!.value(forKey: "message") as! String) //message response message
+                                UIViewController.removeSpinner(spinner: self.sv) //remove spinner
+                                self.displayAlert(title: title, message: message)
+                            } else {
+                                let entryCount = (json!.value(forKey: "entryCount") as! Int)
+                                UIViewController.removeSpinner(spinner: self.sv)
+                                self.defaults.set(entryCount, forKey: "entryCount")
+                                DispatchQueue.main.async { [unowned self] in
+                                    self.navigationController?.popViewController(animated: true)
+                                }
+                            }
+                        } catch {
+                            print("Error deserializing JSON: \(error)")
+                        }
+                    }
+                    task.resume()
+                })
+            }
         } else {
             displayAlert(message: "Failed to find your location, please make sure location is enabled for this app.")
         }
@@ -182,21 +159,6 @@ class BaseWaitTimeController: UIViewController, UITextFieldDelegate, UITextViewD
         guard let locValue: CLLocationCoordinate2D = manager.location?.coordinate else { return }
         print("locations = \(locValue.latitude) \(locValue.longitude)")
         self.currentLocation = locValue
-//        let userLocation = locations.first
-//        if let distance = userLocation?.distance(from: venueLocation) {
-//            if (distance <= radius) {
-//                submitToDatabase()
-//            } else if defaults.object(forKey: "LocationEnabled") != nil && (defaults.object(forKey: "LocationEnabled") as! Bool) == false {
-//                //Check if we bypass location restriction in developer options
-//                submitToDatabase()
-//            } else {
-//                displayAlert(message: "You must be at the venue location to submit an entry.")
-//                UIViewController.removeSpinner(spinner: self.sv)
-//            }
-//        } else {
-//            displayAlert(message: "Unable to get your location. Please make sure location is enabled.")
-//            UIViewController.removeSpinner(spinner: self.sv)
-//        }
     }
     
     func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
@@ -206,77 +168,6 @@ class BaseWaitTimeController: UIViewController, UITextFieldDelegate, UITextViewD
             UIViewController.removeSpinner(spinner: self.sv)
         }
     }
-    
-    func submitToDatabase() {
-        let username = self.defaults.object(forKey: "username") as! String
-        let uid = self.defaults.object(forKey: "userId") as! String
-        let comment = self.comments.text!
-        
-        //Request a timestamp from the cloud
-        let task = URLSession.shared.dataTask(with: timeStampUrl!) {(data, response, error) in
-            do {
-                let json = try JSONSerialization.jsonObject(with: data!) as? NSDictionary
-                let currentTimestamp = (json!.value(forKey: "timestamp") as! Double) //current timestamp
-                var timeDiff = 0.0
-                
-                //Retreive User information to check for 30 minute validation
-                self.ref.child("Users").child(uid).observeSingleEvent(of: .value) { (snapshot) in
-                    var canSubmit = false
-                    var entryCount = snapshot.childSnapshot(forPath: "entryCount").value as! Int
-                    if snapshot.childSnapshot(forPath: "Entries/\(self.name)").hasChildren() {
-                        //We have a past entry for this venue
-                        let pastEntry = snapshot.childSnapshot(forPath: "Entries/\(self.name)").value as! [String: Double]
-                        var postID = ""
-                        var lastPostedTime = 0.0
-                        for (key, value) in pastEntry {
-                            postID = key
-                            lastPostedTime = value
-                        }
-                        print(postID)
-                        timeDiff = currentTimestamp - lastPostedTime
-                        
-                        //If they've posted more than 30 minutes ago, good to go
-                        if timeDiff >= self.timeInterval {
-                            canSubmit = true
-                        }
-                    } else {
-                        //We have no past entry for this venue, good to go
-                        canSubmit = true
-                    }
-                    
-                    if canSubmit {
-                        //Create a dictionary of items to upload
-                        var items = ["Username": username, "Time Stamp": currentTimestamp, "Comment": comment] as [String : Any]
-                        items = self.addItemsToSubmit(items: items)
-                        
-                        //Upload to firebase
-                        self.ref.child("Categories").child(self.categoryType).child(self.name).child("Entries").childByAutoId().setValue(items) { (error, reference) in
-                            //Add entry to their account for rewards as well as the venue, push ID and timestamp
-                            let pushID = reference.key
-                            let items = [pushID: currentTimestamp]
-                            self.ref.child("Users").child(uid).child("Entries").child(self.name).setValue(items) {(error, reference) in
-                                entryCount += 1
-                                self.ref.child("Users").child(uid).child("entryCount").setValue(entryCount, withCompletionBlock: { (error, reference) in
-                                    UIViewController.removeSpinner(spinner: self.sv)
-                                    self.defaults.set(entryCount, forKey: "entryCount")
-                                    self.navigationController?.popViewController(animated: true)
-                                })
-                            }
-                        }
-                    } else {
-                        UIViewController.removeSpinner(spinner: self.sv)
-                        self.displayAlert(message: "Entries can only be made every 30 minutes for a single location. You have \(Int((self.timeInterval - timeDiff)/60.0)) minutes remaining here.")
-                    }
-                }
-            } catch {
-                print("Error deserializing JSON: \(error)")
-                UIViewController.removeSpinner(spinner: self.sv)
-            }
-    
-        }
-        task.resume()
-    }
-    
     
     func addItemsToSubmit(items: [String: Any]) -> [String: Any] {
         return items
